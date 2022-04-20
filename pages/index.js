e513673d70a2a4f30d359702f1115bbd0457c38a
Supercip971 <supercyp971@gmail.com>
2022-04-20 23:41:29 +0000

    misc: fix responsiveness

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Computer from "../components/common/computer";
 import { BottomBar } from "../components/common/bottom";
 export default function Home() {
     return (
-        <div className="w-screen bg-black">
+        <div className=" w-full bg-black">
             <div className=" hidden bsm:flex m-auto text-3xl font-bold max-w-fit back bg-white text-slate-900 flex-row px-8">
                 <h1 className="p-8 hover:bg-black hover:text-white">CYP</h1>
                 <h1 className="p-8 hover:bg-black hover:text-white">BLOG</h1>
@@ -29,7 +29,7 @@ export default function Home() {
                     </h3>
                 </div>
             </div>
-            <div className=" w-screen back text-black bg-white pb-64  px-16">
+            <div className=" w-full back text-black bg-white pb-64 p-8">
                 <div className="max-w-prose m-auto">
                     <h1 className="py-8 font-bold text-4xl">whoami</h1>
                     <h3 className="text-2xl">
