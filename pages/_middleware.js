108f8aaf6a7b7d9fb1173ae2be04e6f4b3a3cdd1
Supercip971 <supercyp971@gmail.com>
2022-04-21 18:10:31 +0000

    misc: compress even more computer textures

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,5 @@ import { NextResponse } from "next/server";
 
 export default function middleware(request) {
     const response = NextResponse.next();
-    console.log(request);
-    console.log(response);
     return response;
 }
