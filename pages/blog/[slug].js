f771186666e8c4da35f04f04a1ce87a91b88e279
Supercip971 <supercyp971@gmail.com>
2022-06-03 12:44:27 +0000

    fix og:image path

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -92,7 +92,7 @@ export default function Post({ front, slug, mdx, source }) {
                 <meta name="author" content="cyp" />
                 <meta property="og:type" content="article" />
                 <meta property="og:site_name" content="cyp" />
-                <meta property="og:image" content={front.socialImage} />
+                <meta property="og:image" content={"/" + front.socialImage} />
                 <meta property="og:description" content={front.description} />
                 <meta property="og:title" content={front.title} />
                 <meta
