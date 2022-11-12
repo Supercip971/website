14a71ac115a73b53200a01991ac93dd659318f94
Supercip971 <supercyp971@gmail.com>
2022-11-12 18:06:15 -0400

    style: now use the Inter font by default.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
+import { Inter } from '@next/font/google'
+
+const inter = Inter();
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return < >
+    <div className={inter.className}>
+
+      <Component {...pageProps} />
+    </div>
+  </>
 }
 
 export default MyApp
