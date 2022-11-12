aec05a7f5e512b9c74acd7a033ee30912e3bd52b
Supercip971 <supercyp971@gmail.com>
2022-11-12 18:04:44 -0400

    components: reworked top navigation bar layout - Also fixed accessibility issues like 'title' for buttons

--- components/common/top.js
+++ components/common/top.js
@@ -34,28 +34,30 @@ export function TopBar(props) {
     };
     return (
         <div>
-            <div className="fixed z-[10] w-screen m-auto text-2xl font-bold hidden bsm:flex back bg-white/90 backdrop-blur-lg  text-slate-900 ">
+            <div className="fixed z-[10] py-2 w-screen m-auto text-2xl font-bold hidden bsm:flex back bg-black/90 backdrop-blur-lg  text-white ">
                 <TopMenu
                     className="w-fit m-auto bsm:flex flex-row "
                     ref={null}
                 />
             </div>
-            <div className="fixed z-[10] w-screen m-auto text-2xl font-bold flex flex-col bsm:hidden back bg-white/90 backdrop-blur-lg  text-slate-900 ">
+            <div className="fixed z-[10] w-screen m-auto text-2xl font-bold flex flex-col bsm:hidden back bg-black/90 backdrop-blur-lg  text-white ">
                 <div className="w-full m-auto flex flex-row ">
-                    <h1 className="py-8 px-16 hover:bg-black hover:text-white cursor-pointer w-max">
-                        CYP
+                    <h1 className="py-4 px-16 hover:bg-black hover:text-white cursor-pointer w-max text-white ">
+                        Home
                     </h1>
                     <button
-                        className="py-8 px-16 ml-auto bg-transparent hover:bg-black  cursor-pointer w-fit"
+                        className="py-4 px-16 ml-auto bg-transparent hover:bg-white bg-black hover:text-black text-white  cursor-pointer w-fit"
                         ref={barref}
                         onClick={onclickfn}
+                        title="open main menu"
                     >
                         <svg
                             height="100%"
                             viewBox="0 0 178 178"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
-                            className="fill-black hover:fill-white pr-8"
+
+                            className="pr-8 fill-current"
                         >
                             <rect x="15" y="29" width="148" height="24" />
                             <rect x="15" y="77" width="148" height="24" />
@@ -64,8 +66,8 @@ export function TopBar(props) {
                     </button>
                 </div>
 
-                <div ref={menuref} className="hidden w-screen">
-                    <TopMenu className="w-full m-auto flex-col flex px-16" />
+                <div ref={menuref} className="hidden w-screen ease-in-out duration-300">
+                    <TopMenu className="w-full m-auto flex-col flex p-8" />
                 </div>
             </div>
         </div>
