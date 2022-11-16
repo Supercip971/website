50acda9ea9dfa7f03633562cf8a90fc3314ccf3e
Supercip971 <supercyp971@gmail.com>
2022-11-16 18:38:19 -0400

    misc: re-add sitemap.xml generation Because google don't seem to want to index my page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
     siteUrl: process.env.SITE_URL || "https://cyp.sh",
+    generateRobotsTxt: true, 
 };
 
 module.exports = config;
